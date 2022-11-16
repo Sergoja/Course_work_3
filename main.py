7735d006682a52860e129a3b79db00c83ebe8d85
Sergo <sergojadesu@gmail.com>
2022-11-16 23:57:48 +1000

    Project changed

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template
-
+from utils import get_comments_by_post_id, get_posts_all
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def page()
+def main_page():
+    all_posts = get_posts_all()
+    return render_template('index.html', all_posts=all_posts)
+
+
+@app.route('/posts/<int:post_id>')
+def post_page(post_id):
+    comments_by_post_id = get_comments_by_post_id(post_id)
+    all_posts = get_posts_all()
+    required_post = {}
+    for post in all_posts:
+        if int(post['pk']) == int(post_id):
+            required_post = post
+    return render_template('post.html', comments=comments_by_post_id, required_post=required_post)
+
+
+@app.route('/search')
+def search_page():
+    return render_template('search.html')
+
+
+@app.route('/user-feed')
+def user_feed_page():
+    return render_template('user-feed.html')
+
+
+app.run()
--- utils.py
+++ utils.py
@@ -1,5 +1,21 @@
+import json
 
-# get_posts_all()
+
+def get_posts_all():
+    with open("data/posts.json", 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def get_comments_by_post_id(post_id):
+    with open("data/comments.json", 'r', encoding='utf-8') as file:
+        comments_by_post_id = []
+        for comment in json.load(file):
+            if int(comment['post_id']) == int(post_id):
+                comments_by_post_id.append(comment)
+    return comments_by_post_id
+
+
+
 
 # get_posts_by_user(user_name) – возвращает посты определенного пользователя.
 # Функция должна вызывать ошибку ValueError если такого пользователя нет и пустой список,
